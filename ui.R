a60bbea334ee9723f72cf06045758a31240a1d15
Huascar Duarte <huascar.duarte@gmail.com>
2014-10-26 13:00:55 -0200

    First change on prediction model

--- ui.R
+++ ui.R
@@ -1,25 +1,15 @@
-shinyUI(bootstrapPage(
-    
-    selectInput(inputId = "n_breaks",
-                label = "Number of bins in histogram (approximate):",
-                choices = c(10, 20, 35, 50),
-                selected = 20),
-    
-    checkboxInput(inputId = "individual_obs",
-                  label = strong("Show individual observations"),
-                  value = FALSE),
-    
-    checkboxInput(inputId = "density",
-                  label = strong("Show density estimate"),
-                  value = FALSE),
-    
-    plotOutput(outputId = "main_plot", height = "300px"),
-    
-    # Display this only if the density is shown
-    conditionalPanel(condition = "input.density == true",
-                     sliderInput(inputId = "bw_adjust",
-                                 label = "Bandwidth adjustment:",
-                                 min = 0.2, max = 2, value = 1, step = 0.2)
+library(shiny)
+shinyUI( pageWithSidebar(
+    # Application title 
+    headerPanel("Diabetes prediction"),
+    sidebarPanel(
+        numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5), 
+        submitButton('Submit')
+    ), 
+    mainPanel(
+        h3('Results of prediction'),
+        h4('You entered'), verbatimTextOutput("inputValue"), 
+        h4('Which resulted in a prediction of '), verbatimTextOutput("prediction")
+        ) 
     )
-    
-))
+)
